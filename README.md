f40d2adba4610c8a51220eb7eabc0412058faaf2
Admin <lamhaison@gmail.com>
2023-01-30 16:11:17 +0700

    [init] - init source and add peco history commandline

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # helpful-commandlines
 This is the repo to collect helpful commandlines that is used for mac and linux os.
+
+## Setup dependencies
+Notes: This document is for macos environment.
+
+### Install peco
+To allow searching by console.
+![image](./images/peco_history_menu.png)
+
+```
+brew install peco
+peco --version
+peco version v0.5.10 (built with go1.19.2)
+```
+
+## Settings when open terminal (I am using iterm)
+```
+echo "source /opt/lamhaison-tools/helpful-commandlines/main.sh" >> ~/.bashrc
---
+++ main.sh
@@ -0,0 +1,18 @@
+# Get the current directory of the main.sh script.
+export HELPFUL_COMMANDLINES_SOURCE_SCRIPTS="$(dirname -- "$0")"
+
+# Import sub-commandline.
+# https://yukimemi.netlify.app/all-you-need-is-peco/
+# https://thevaluable.dev/zsh-line-editor-configuration-mouseless/
+for module in $(echo "common"); do
+	for script in $(ls ${HELPFUL_COMMANDLINES_SOURCE_SCRIPTS}/${module}); do
+		source ${HELPFUL_COMMANDLINES_SOURCE_SCRIPTS}/${module}/${script}
+	done
+done
+
+# Add hot-keys
+zle -N peco_select_history
+bindkey '^r' peco_select_history
+
+# zle -N aws_assume_role_set_name_with_hint
+# bindkey '^@' aws_assume_role_set_name_with_hint
