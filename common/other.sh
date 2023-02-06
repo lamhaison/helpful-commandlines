e94ab6dbeb07d20c33a715fdcc9ef9cf3f82e646
Admin <lamhaison@gmail.com>
2023-02-06 17:35:33 +0700

    [update] - add instruction for installing clipy on macos

---
+++ common/other.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function lamhaison_help_install_macos_clipboard() {
+	cat <<-_EOF_
+		Install clipy
+		Access Shift + command + v
+	_EOF_
+}
+
+function lamhaison_help_install_macos_peco() {
+	cat <<-_EOF_
+		# Install peco
+		brew install peco
+	_EOF_
+
+}
