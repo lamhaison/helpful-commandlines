f7ec3c31750b307f803175a1e2a2ac999926d384
Admin <lamhaison@gmail.com>
2023-02-07 17:54:22 +0700

    [update] - add github cli commandline

--- common/other.sh
+++ common/other.sh
@@ -40,3 +40,50 @@ function lamhaison_git_tag_create() {
 	git checkout tags/${tag_version}
 	# git diff --name-only v1.0.2 v1.0.3
 }
+
+function lamhaison_project_get() {
+	input_project=$(peco_repo_list)
+	cd ${input_project}
+}
+
+function lamhaison_vi_set_commandlines() {
+	cat <<-_EOF_
+		: set nu
+		: set nu! or :set nonu
+	_EOF_
+}
+
+function lamhaison_github_repo_list() {
+	local git_owner=$1
+	gh repo list ${git_owner:?'git_owner is unset or empty'} \
+		--visibility private --json name --jq '.[].name'
+}
+
+function lamhason_github_repo_list_sshurl() {
+	local git_owner=$1
+	gh repo list ${git_owner:?'git_owner is unset or empty'} \
+		--visibility private --json sshUrl --jq '.[].sshUrl'
+}
+
+function lamhason_github_repo_get_collaborators() {
+	local git_repo_name=$1
+	# echo "Get collaborators of the repo ${git_repo_name}"
+	gh api -H "Accept: application/vnd.github+json" \
+		/repos/${git_repo_name:?'git_repo_name is unset or empty'}/collaborators
+}
+
+function lamhason_github_repo_clone() {
+	local git_repo_name=$1
+	gh repo clone ${git_repo_name:?'git_repo_name is unset or empty'}
+}
+
+function lamhaison_github_clone_all() {
+	local owner_list=${1}
+	for owner in $(echo ${owner_list:?'owner_list is unset or empty'}); do
+		mkdir -p ${LAMHAISON_PROJECTS_DIR}/${owner}
+		cd ${LAMHAISON_PROJECTS_DIR}/${owner}
+		for repo in $(lamhaison_github_repo_list ${owner}); do
+			lamhason_github_repo_clone "${owner}/${repo}"
+		done
+	done
+}
