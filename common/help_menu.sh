d0ba295e163ef4253bc8d7fde2adaedb990bdd07
Admin <lamhaison@gmail.com>
2023-02-16 13:49:14 +0700

    [add] - add peco functions to menu search

--- common/help_menu.sh
+++ common/help_menu.sh
@@ -3,7 +3,7 @@ lamhaison_help_helpful() {
 	# Support both function function_name() { or function_name with prefix aws_bla_bla() {
 	BUFFER=$(
 		cat $(find ${HELPFUL_COMMANDLINES_SOURCE_SCRIPTS} -type f -name '*.sh' | grep -v main.sh) |
-			grep -e "^function.*\(.+*\)" -e "^aws*\(.+*\)" | tr -d "(){" | awk -F ' ' '{ print ($1=="function") ? $2 : $1}' | sort | peco --query "$LBUFFER"
+			grep -e "^function.*\(.+*\)" -e "^aws*\(.+*\)" -e "^peco*\(.+*\)" | tr -d "(){" | awk -F ' ' '{ print ($1=="function") ? $2 : $1}' | sort | peco --query "$LBUFFER"
 	)
 
 	CURSOR=$#BUFFER
@@ -13,9 +13,10 @@ lamhaison_help_helpful() {
 lamhaison_help() {
 
 	# Support both function function_name() { or function_name with prefix aws_bla_bla() {
+	function_list=
 	BUFFER=$(
 		cat $(find ${LAMHAISON_HELPFUL_LOOKUP} -type f -name '*.sh' | grep -v main.sh) |
-			grep -e "^function.*\(.+*\)" -e "^aws*\(.+*\)" | tr -d "(){" | awk -F ' ' '{ print ($1=="function") ? $2 : $1}' | sort | peco --query "$LBUFFER"
+			grep -e "^function.*\(.+*\)" -e "^aws*\(.+*\)" -e "^peco*\(.+*\)" | tr -d "(){" | awk -F ' ' '{ print ($1=="function") ? $2 : $1}' | sort | peco --query "$LBUFFER"
 	)
 
 	CURSOR=$#BUFFER
