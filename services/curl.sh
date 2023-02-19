7c156847895a7a8a7c736d2a2c87d79ce74c0cf0
lamhaison <lamhaison@gmail.com>
2023-02-20 00:20:14 +0700

    [Aadd] - add the function to get http response without content

---
+++ services/curl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function lhs_curl_get_response_headers_only() {
+
+	lhs_curl_url=${1:='https://devopsmountain.com'}
+	echo "\
+		http_endpoint: ${lhs_curl_url}
+		-s hides the progress bar
+		-D - dump headers to stdout indicated by -
+		-o /dev/null send output (HTML) to /dev/null essentially ignoring it
+	"
+
+	curl -s -D - -o /dev/null ${lhs_curl_url}
+}
