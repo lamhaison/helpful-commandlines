1c3382f5c9193f0dfc912d91229b61f5e65be266
lamhaison <lamhaison@gmail.com>
2023-02-20 20:33:26 +0700

    [Add] - add function to create release version file - lhs_jenkins_create_release_version_file

--- services/jenkins.sh
+++ services/jenkins.sh
@@ -3,3 +3,11 @@
 function lhs_jenkins_load_helpful_commandlines() {
 	for script in $(find ${HELPFUL_COMMANDLINES_SOURCE_SCRIPTS:=~/lamhaison-tools/helpful-commandlines} -type f -name '*.sh' | grep -v main.sh); do source $script; done
 }
+
+function lhs_jenkins_create_release_version_file() {
+	VERSION_FILE=./release.txt
+	release_time=$(date '+%Y-%m-%d %H:%M:%S')
+	echo "release_hash: ${GIT_COMMIT}" >${VERSION_FILE}
+	echo "released_at: ${release_time}" >>${VERSION_FILE}
+	cat ${VERSION_FILE}
+}
