6c77a9889f3ddd913208b26a864256bc33d0f95e
lamhaison <lamhaison@gmail.com>
2023-02-24 10:15:29 +0700

    [Refactor] - add local when define variables for lhs_ci_laravel_* function

--- services/ci_cd.sh
+++ services/ci_cd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function lhs_ci_laravel_replace_aws_credentials_in_env_file() {
-	APP_ENV=$1
-	OLD_AWS_ACCESS_KEY_ID=$2
-	NEW_AWS_ACCESS_KEY_ID=$3
-	OLD_AWS_SECRET_ACCESS_KEY=$4
-	NEW_AWS_SECRET_ACCESS_KEY=$5
+	local APP_ENV=$1
+	local OLD_AWS_ACCESS_KEY_ID=$2
+	local NEW_AWS_ACCESS_KEY_ID=$3
+	local OLD_AWS_SECRET_ACCESS_KEY=$4
+	local NEW_AWS_SECRET_ACCESS_KEY=$5
 	cat .env.${APP_ENV} |
 		sed "s|${OLD_AWS_ACCESS_KEY_ID}|${NEW_AWS_ACCESS_KEY_ID}|g" |
 		sed "s|${OLD_AWS_SECRET_ACCESS_KEY}|${NEW_AWS_SECRET_ACCESS_KEY}|g" \
@@ -14,12 +14,12 @@ function lhs_ci_laravel_replace_aws_credentials_in_env_file() {
 }
 
 function lhs_ci_laravel_overwrite_aws_credentials_in_env_file() {
-	APP_ENV=$1
+	local APP_ENV=$1
 	rm -rf .env.${APP_ENV}
 	mv .env.${APP_ENV}_tmp .env.${APP_ENV}
 }
 
 function lhs_ci_laravel_delete_aws_credentials_in_env_file() {
-	APP_ENV=$1
+	local APP_ENV=$1
 	rm -rf .env.${APP_ENV}
 }
