e9e695a046014475018f1b2d6227e16b5b5d253f
lamhaison <lamhaison@gmail.com>
2023-02-27 06:21:51 +0700

    [Add] - to add function for how to name a function(get, list, add, rm)

--- services/other.sh
+++ services/other.sh
@@ -6,6 +6,15 @@ function lhs_code_commment_instruction() {
 	__EOF__
 }
 
+function lhs_code_function_name_instruction() {
+	cat <<__EOF__
+		get: to get an item.
+		list: to list all items.
+		rm: to remove the item.
+		add: to add an item.
+__EOF__
+}
+
 function lhs_vi_set_commandlines_instruction() {
 	cat <<-_EOF_
 		: set nu
