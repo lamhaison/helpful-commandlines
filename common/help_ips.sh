df8b1f2ade9bf66bef4a16984974e34bba8a17ff
lamhaison <lamhaison@gmail.com>
2023-02-28 22:24:34 +0700

    [Refactor] - to refactor function name

--- common/help_ips.sh
+++ common/help_ips.sh
@@ -1,10 +1,14 @@
-function lhs_help_get_public_ip() {
+function lhs_network_get_public_ip_instruction() {
 	echo "
 		dig +short myip.opendns.com @resolver1.opendns.com
 	"
 
 }
 
-function lhs_get_public_ip() {
+function lhs_network_get_public_ip() {
 	dig +short myip.opendns.com @resolver1.opendns.com
+}
+
+function lhs_what_is_my_ip() {
+	lhs_network_get_public_ip
 }
