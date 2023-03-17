1ec67f1233bafc89f21a86c1f686a7e2db54b19b
Admin <lamhaison@gmail.com>
2023-03-17 16:45:22 +0700

    [Refactor] - to hide private functions when creating function name search menu

--- common/peco/other.sh
+++ common/peco/other.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 function lhs_peco_function_list() {
+	# Ignore private function
 	local lhs_function_list_cmd="
-		find ${LHS_HELPFUL_LOOKUP} -type f -name '*.sh' | grep -v main.sh | xargs cat | grep -e '^function.*\(.+*\)' -e '^aws*\(.+*\)' -e '^peco*\(.+*\)' -e '^lhs*\(.+*\)' \
+		find ${LHS_HELPFUL_LOOKUP} -type f -name '*.sh' | grep -v main.sh | xargs cat | grep -v '^function.*private.*' \
+		| grep -e '^function.*\(.+*\)' -e '^aws*\(.+*\)' -e '^peco*\(.+*\)' -e '^lhs*\(.+*\)' \
 		| tr -d '(){' | awk -F ' ' '{ print (\$1==\"function\") ? \$2 : \$1}' | sort
 	"
 	# Cache in 1 minute
