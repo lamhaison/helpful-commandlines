cada3866d27dc1b5e9539b3e5e734480259ace53
lamhaison <lamhaison@gmail.com>
2023-07-30 10:06:57 +0700

    [Improvement] - optimize when finding file

--- common/peco/other.sh
+++ common/peco/other.sh
@@ -3,7 +3,10 @@
 function lhs_peco_function_list() {
 	# Ignore private function
 	local lhs_function_list_cmd="
-		find ${LHS_HELPFUL_LOOKUP} -type f -name '*.sh' | grep -v main.sh | xargs cat | grep -v '^function.*private.*' \
+		find ${LHS_HELPFUL_LOOKUP} -type f -name '*.sh' \
+			-not -path \"${LHS_HELPFUL_LOOKUP}/private-helpful-commandlines/tmp/*\" \
+			-not -path \"${LHS_HELPFUL_LOOKUP}/dotfiles/*\" \
+		| grep -v main.sh | xargs cat | grep -v '^function.*private.*' \
 		| grep -e '^function.*\(.+*\)' -e '^aws*\(.+*\)' -e '^peco*\(.+*\)' -e '^lhs*\(.+*\)' \
 		| tr -d '(){' | awk -F ' ' '{ print (\$1==\"function\") ? \$2 : \$1}' | sort
 	"
