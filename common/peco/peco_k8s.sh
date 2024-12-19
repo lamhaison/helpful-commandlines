31944e21efada07562f1f06d05fa2061264f50a5
lamhaison <lamhaison@gmail.com>
2024-12-19 16:25:15 +0700

    [Add] - reformat functions and add function for list k8s daemonsets

--- common/peco/peco_k8s.sh
+++ common/peco/peco_k8s.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
-peco_k8s_input() {
+function peco_k8s_input() {
 	lhs_peco_commandline_input "${1}" 'true'
 }
 
-peco_k8s_namespace_list() {
+function peco_k8s_namespace_list() {
 	peco_k8s_input "kubectl get namespaces | awk -F ' ' '{print \$1}' | grep -v 'NAME'"
 
 }
 
-peco_k8s_deployment_list() {
+function peco_k8s_deployment_list() {
 	peco_k8s_input "kubectl get deployments --all-namespaces \
 		| awk -F ' ' 'BEGIN { OFS=\"  |   \" } { print \$2,\$1}' | grep -v 'NAME'"
 }
 
-peco_k8s_pod_list() {
+function peco_k8s_pod_list() {
 	peco_k8s_input "kubectl get pods --all-namespaces \
 		| awk -F ' ' 'BEGIN { OFS=\"  |   \" } { print \$2,\$1}' | grep -v 'NAME'"
 }
 
-peco_k8s_pod_list_reloaded() {
+function peco_k8s_pod_list_reloaded() {
 	lhs_peco_commandline_input "kubectl get pods --all-namespaces \
 		| awk -F ' ' 'BEGIN { OFS=\"  |   \" } { print \$2,\$1}' | grep -v 'NAME'" 'false'
 }
 
-peco_k8s_ingress_list() {
+function peco_k8s_ingress_list() {
 	peco_k8s_input "kubectl get ingress --all-namespaces \
 		| awk -F ' ' 'BEGIN { OFS=\"  |   \" } { print \$2,\$1}' | grep -v 'NAME'"
 
+}
+
+function peco_k8s_daemonset_list() {
+	peco_k8s_input "kubectl get daemonset --all-namespaces \
+		| awk -F ' ' 'BEGIN { OFS=\"  |   \" } { print \$2,\$1}' | grep -v 'NAME'"
 }
